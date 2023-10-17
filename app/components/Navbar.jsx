1ec8a8d1bfedf110117386bf831c6e04ee07da8e
JoelAngel <joelbaraka4325@gmail.com>
2023-10-17 16:10:09 +0300

    styles, font, images

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/dojo-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <h1>Joel Angel</h1>
+      {/* height is not need, the width on its own will auto scale the height */}
+      <Image
+        src={Logo}
+        alt="Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Home</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">tickets</Link>
     </nav>
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,42 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <h2>DashBaord</h2>
+    <main>
+      <h2>Dashboard</h2>
+      <p>
+        1 The LORD is my shepherd, I lack nothing. 2 He makes me lie down in
+        green pastures, he leads me beside quiet waters, 3 he refreshes my soul.
+        He guides me along the right paths for his name’s sake. 4 Even though I
+        walk through the darkest valley,I will fear no evil, for you are with
+        me; your rod and your staff, they comfort me. 5 You prepare a table
+        before me in the presence of my enemies. You anoint my head with oil; my
+        cup overflows. 6 Surely your goodness and love will follow me all the
+        days of my life, and I will dwell in the house of the LORD forever.
+      </p>
+
+      <div className="flex justify-center my-8">
+        <Link href="/tickets">
+          <button className="btn-primary">View Tickets</button>
+        </Link>
+      </div>
+
+      <h2>Company Updates</h2>
+
+      <div className="card">
+        <h3>New member of the web dev team...</h3>
+        <p>
+          Study to shew thyself approved unto God, a workman that needeth not to
+          be ashamed, rightly dividing the word of truth.
+        </p>
+      </div>
+      <div className="card">
+        <h3>New website live!</h3>
+        <p>
+          Study to shew thyself approved unto God, a workman that needeth not to
+          be ashamed, rightly dividing the word of truth.
+        </p>
+      </div>
     </main>
   );
 }
